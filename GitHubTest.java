a4f042d2713d8c32cdae3c2029e15cf794becd73
kiraess <karrina.xie@gmail.com>
2017-11-20 10:56:12 -0500

    Added println

--- GitHubTest.java
+++ GitHubTest.java
@@ -2,5 +2,7 @@ public class GitHubTest{
   pubilc static void main(String[] args){
     TextIO.putf("Hello World!");
     System.out.println("holaaaa");
+    System.out.println("Another one");
   }
+
 }
